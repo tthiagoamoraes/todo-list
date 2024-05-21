dedef650432b4408db693000253ed5d5d36d3708
hypre019 <tthiago.amorim@gmail.com>
2024-05-20 22:32:28 -0300

    first commit

---
+++ src/components/Todo.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Todo = ({ todo }) => {
+  return (
+    <div>
+      <div className="todo">
+            <div className="content">
+              <p>{todo.text}</p>
+              <p className="categoria">({todo.category})</p>
+            </div>
+            <div>
+              <button className="complete">Complentar</button>
+              <button className="remove">x</button>
+            </div>
+          </div>
+    </div>
+  )
+}
+
+export default Todo
